96b976fc6cb32edd0938de543331e1e6b807d27f
bubblesort <aleksanrmerklin@gmail.com>
2020-07-13 19:15:51 +0400

    //

---
+++ webpack.dev.conf.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseWebpackConfig = require('./webpack.base.config');
+
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+    mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: baseWebpackConfig.externals.paths.dist,
+        port: 8081,
+        overlay: {
+            warnings: true,
+            errors: true
+        },
+        // historyApiFallback: true,
+        // proxy: {
+        //     '/api': {
+        //         target: 'http://project-mentor.ru',
+        //         pathRewrite: {'^/api' : ''},
+        //         changeOrigin: true,
+        //         secure: false
+        //     },
+        // }
+    },
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: '[file].map'
+        })
+    ]
+});
+
+module.exports = new Promise((resolve, reject) => {
+    resolve(devWebpackConfig)
+});
+
+
+
+
